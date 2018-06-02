d2b1f3b736e65a0122ef856cf6e84d9297a22096
Tiago Rosendo <tiago@MacBook-Pro-2.local>
2018-06-02 01:01:04 -0300

    Jalim was born

---
+++ jalim.js
@@ -0,0 +1,36 @@
+const botBuilder = require('botbuilder');
+const restify = require('restify');
+require('dotenv').load();
+
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 4141, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat bot
+var connector = new botBuilder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+var bot = new botBuilder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+const LuisModelUrl = process.env.LUIS_MODEL_URL;
+
+// Main dialog with LUIS
+var recognizer = new botBuilder.LuisRecognizer(LuisModelUrl);
+
+var intents = new botBuilder.IntentDialog({ recognizers: [recognizer] })
+
+    // If User greeted the bot
+    .matches('greeting', (session, args) => {
+        session.send('Hi There');
+    }).onDefault((session) => {
+        session.send('Sorry, I did not understand.', session.message.text);
+    });
+
+
+bot.dialog('/', intents);
